0a76e9e84a90195dfb2c26596e83b244fea93ec2
Aastha <aastha1406@gmail.com>
2024-09-07 18:52:05 +0530

    constructor injection concept

---
+++ src/main/java/com/aastha/firstApp/Coach.java
@@ -0,0 +1,9 @@
+package com.aastha.firstApp;
+
+// IOC - inversion of control - dependency injection
+
+// Step 1 - creating interface
+public interface Coach {
+    String getDailyWorkout();
+}
+
---
+++ src/main/java/com/aastha/firstApp/DemoRestController.java
@@ -0,0 +1,22 @@
+package com.aastha.firstApp;
+
+// Step 3: create RestController
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestController {
+//    3.1 Define a private field for dependency
+    private Coach myCoach;
+//    3.2 define a constructor for dependency injection using @Autowired
+    @Autowired
+    public DemoRestController(Coach myCoach){
+        this.myCoach = myCoach;
+    }
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
