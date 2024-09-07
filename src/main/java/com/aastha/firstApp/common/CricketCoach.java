aa3c0cdbea52ff0d832de2432a8468b34117af92
Aastha <aastha1406@gmail.com>
2024-09-07 19:58:50 +0530

    code cleanup

--- src/main/java/com/aastha/firstApp/common/CricketCoach.java
+++ src/main/java/com/aastha/firstApp/common/CricketCoach.java
@@ -3,23 +3,12 @@ package com.aastha.firstApp.common;
 // Step 2 - create class that implements the interface
 
 import com.aastha.firstApp.Coach;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component // marks class as Spring Bean and makes it available for dependency injection
 public class CricketCoach implements Coach {
     public CricketCoach(){
         System.out.println("In class " + getClass().getSimpleName());
-    }
-
-    @PostConstruct
-    public void doAfterCreation(){
-        System.out.println(getClass().getSimpleName() + " is created");
-    }
-    @PreDestroy
-    public void doCleanup(){
-        System.out.println(getClass().getSimpleName() + " is destroyed");
     }
     // it is mandatory to override all items defined in interface
     @Override
