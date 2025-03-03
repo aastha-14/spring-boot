8f090448e7a834b18adcd6ed919c7dcd58058b4b
Aastha <aastha1406@gmail.com>
2025-03-03 23:12:39 +0530

    execption handling for not foundexception

--- src/main/java/com/aastha/springboot/rest/DemoRestController.java
+++ src/main/java/com/aastha/springboot/rest/DemoRestController.java
@@ -1,7 +1,10 @@
 package com.aastha.springboot.rest;
 
 import com.aastha.springboot.entity.Student;
+import com.aastha.springboot.exception.StudentNotFoundException;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +14,26 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class DemoRestController {
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    // define @PostConstruct to load student data.. only once!
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
         students.add(new Student("Aastha", "Gupta", "aastha@gmail.com"));
         students.add(new Student("John", "Doe", "johndoe@ggmail.com"));
+    }
+    @GetMapping("/students")
+    public List<Student> getStudents(){
         return students;
+    }
+
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudentById(@PathVariable int studentId){
+        if(studentId > students.size() || studentId < 0){
+            throw new StudentNotFoundException("Student ID not found: " + studentId);
+        }
+        return students.get(studentId);
     }
 }
